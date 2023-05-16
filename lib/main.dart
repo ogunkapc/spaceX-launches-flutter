00de13a0c0f0f5fc9b99cea7dccf7053ae5438ab
Archer <ogunkapc@gmail.com>
2023-05-16 23:49:33 +0100

    UI

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:spacex_launches/api_service.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // String data = "";
+  Future<List>? _launches;
+  bool _isDataLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future _fetchData() async {
+    final future = ApiService.get();
+    setState(() {
+      _launches = future;
+      _isDataLoaded = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SpaceX API Demo'),
+      ),
+      backgroundColor: Colors.blue[50],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Begin the API request"),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    // data = ApiService().get().toString();
+                    _fetchData();
+                  });
+                },
+                child: const Text("Load data"),
+              ),
+              const SizedBox(height: 20),
+              if (_isDataLoaded)
+                FutureBuilder(
+                  future: _launches,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      final launches = snapshot.data!;
+                      print(launches);
+                      return Expanded(
+                        child: ListView.builder(
+                            itemCount: launches.length,
+                            itemBuilder: (context, index) {
+                              final missionName =
+                                  launches[index]['mission_name'] ?? 'N/A';
+                              final launchDateUtc =
+                                  launches[index]['launch_date_utc'] ?? 'N/A';
+
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 15.0),
+                                child: Container(
+                                  height: 150,
+                                  // width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8.0, top: 10.0, right: 8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          missionName,
+                                          style: const TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white),
+                                        ),
+                                        const SizedBox(
+                                          height: 8.0,
+                                        ),
+                                        Text(
+                                          // "This task was completed today!! This task was completed today!! This task was completed today!! This task was completed today!! This task was completed today!! This task was completed today!! This task was completed today!! ",
+                                          launchDateUtc,
+                                          style: const TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                              // return ListTile(
+                              //   title: Text(missionName),
+                              //   subtitle: Text(launchDateUtc),
+                              // );
+                            }),
+                        // child: SingleChildScrollView(
+                        // child: Padding(
+                        //   padding: const EdgeInsets.all(16.0),
+                        //   child: Text(snapshot.data ?? "No data"),
+                        // ),
+                        // ),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
